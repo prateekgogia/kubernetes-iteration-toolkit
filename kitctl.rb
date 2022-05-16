477072b03d1b93ef460c34f0c6aa5a7d616404d7
Prateek Gogia <prateekgogia42@gmail.com>
2022-05-16 17:16:42 -0500

    Update kitctl to v0.0.12 Created by Github actions during Release CLI action

--- kitctl.rb
+++ kitctl.rb
@@ -2,10 +2,10 @@
 require 'formula'
 class Kitctl < Formula
   homepage 'https://github.com/awslabs/kubernetes-iteration-toolkit/substrate'
-  version '0.0.11'
+  version '0.0.12'
   if OS.mac? && Hardware::CPU.is_64_bit?
-    url 'https://github.com/awslabs/kubernetes-iteration-toolkit/releases/download/v0.0.11/kitctl_v0.0.11_darwin_amd64.zip'
-    sha256 '44a22c992cce1f449555e759d097caaa6878e8f0b662ada681874c22cb082944'
+    url 'https://github.com/awslabs/kubernetes-iteration-toolkit/releases/download/v0.0.12/kitctl_v0.0.12_darwin_amd64.zip'
+    sha256 'a2514c7f3b2e558c97c7b155f683ccd45aa19b68bac7683069f921e1ea813af7'
   else
     echo "Hardware not supported"
     exit 1
